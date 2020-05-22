44e84b3978e8aa77611162b3a96fd0a4705c12d0
yevhenii <testjokerqwerty@gmail.com>
2020-05-22 21:57:12 +0300

    sum

--- 01_intro/intro.js
+++ 01_intro/intro.js
@@ -1,17 +1,17 @@
-function expect (val) {
-  return {
-    toBe: (exp) => {
-      if (val === exp) {
-        console.log('success')
-      } else {
-        console.error(`error ${val}`)
-      }
-    }
-  }
+function expect(val) {
+   return {
+      toBe: exp => {
+         if (val === exp) {
+            console.log('success');
+         } else {
+            console.error(`error ${val}`);
+         }
+      },
+   };
 }
 
-const sum = (a, b) => a + b+1
-const nativeNull = () => null
-console.log(sum(1, 2))
-module.exports = { sum, nativeNull }
+const sum = (a, b) => a + b;
+const nativeNull = () => null;
+console.log(sum(1, 2));
+module.exports = { sum, nativeNull };
 // expect(sum(1, 3)).toBe(3)
--- 01_intro/intro.test.js
+++ 01_intro/intro.test.js
@@ -1,4 +1,18 @@
 const { sum, nativeNull } = require('./intro')
 test('sum should  return sum of two values', () => {
 expect(sum(2,5)).toBe(7)
+expect(sum(2,5)).toEqual(7)
+})
+
+test('Sum should return value corrrect comparing to other', ()=>{
+expect(sum(3,3)).toBeGreaterThan(5)
+expect(sum(3,3)).toBeGreaterThanOrEqual(5)
+expect(sum(3,3)).toBeLessThan(10)
+expect(sum(3,3)).toBeLessThanOrEqual(6)
+})
+test('sum should sum 2 float values correctly',()=>{
+expect(sum(0.3,0.6)).toBeCloseTo(0.9)
+})
+
+test('Native null should return ',()=>{
 })
