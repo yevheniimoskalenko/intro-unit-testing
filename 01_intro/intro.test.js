d657d22293b636e88be26725936448d6fe414810
yevhenii <testjokerqwerty@gmail.com>
2020-05-22 22:00:00 +0300

    describe

--- 01_intro/intro.test.js
+++ 01_intro/intro.test.js
@@ -1,4 +1,7 @@
 const { sum, nativeNull } = require('./intro')
+
+
+describe('Sum Funct', ()=>{
 test('sum should  return sum of two values', () => {
 expect(sum(2,5)).toBe(7)
 expect(sum(2,5)).toEqual(7)
@@ -13,6 +16,9 @@ expect(sum(3,3)).toBeLessThanOrEqual(6)
 test('sum should sum 2 float values correctly',()=>{
 expect(sum(0.3,0.6)).toBeCloseTo(0.9)
 })
-
-test('Native null should return ',()=>{
 })
+describe('native Funct', ()=>{
+test('Native null should return false value null',()=>{
+})
+})
+
