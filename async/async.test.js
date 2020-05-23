af23458b65e684c0379f3b8ea4fb69133a88d212
yevhenii <testjokerqwerty@gmail.com>
2020-05-23 06:59:53 +0300

    async

--- async/async.test.js
+++ async/async.test.js
@@ -1,3 +1,8 @@
 const Ajax = require('./async');
 
-describe('Ajax: echo');
+describe('Ajax: echo', () => {
+   test('should return value async', async () => {
+      const result = await Ajax.echo('some data');
+      expect(result).toBe('some data');
+   });
+});
